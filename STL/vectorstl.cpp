5dd347d17367ab45644f820963d7105adc420c4d
husainnt <hussaintahir436@gmail.com>
2024-09-19 13:15:41 +0500

    first commit

--- STL/vectorstl.cpp
+++ STL/vectorstl.cpp
@@ -3,33 +3,33 @@
 using namespace std;
 int main()
 {
-    vector<int> v;       // declaring a vector
-    vector<int> a(5, 1); // vector 'a' intialised with size 5 and with 1 as all elements
-    vector<int> z(a);    // all elements of 'a' are copied into z
-    v.push_back(1);      // adding an element
+    vector<int> v;
+    vector<int> a(5, 1);
+    vector<int> z(a);
+    v.push_back(1);
     cout << "Capacity of vector: " << v.capacity() << endl;
-    v.push_back(2); // adding an element
+    v.push_back(2);
     cout << "Capacity of vector: " << v.capacity() << endl;
-    v.push_back(3);                                         // adding an element
-    cout << "Capacity of vector: " << v.capacity() << endl; // now capacity will double and be 4
-    cout << "Size of vector: " << v.size() << endl;         // size will be 3 as 3 elements
+    v.push_back(3);
+    cout << "Capacity of vector: " << v.capacity() << endl;
+    cout << "Size of vector: " << v.size() << endl;
     cout << "Element at 1st index: " << v.at(1) << endl;
     cout << "First element: " << v.front() << endl;
     cout << "Last Element: " << v.back() << endl;
-    // displaying all elements
+
     cout << "Elements in the vector are: " << endl;
     for (int i : v)
     {
         cout << i << " ";
     }
-    // after pop
+
     v.pop_back();
     cout << "Elements in the vector after pop are: " << endl;
     for (int i : v)
     {
         cout << i << " ";
     }
-    v.clear(); // clearing a vector
+    v.clear();
     cout << "Size of vector after clearing :" << v.size() << endl;
 
     return 0;
